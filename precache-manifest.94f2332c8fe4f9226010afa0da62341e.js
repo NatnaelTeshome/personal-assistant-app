dd72da5a1e8df6607d8dc91200f260d1c43785a6
NatnaelTeshome <natnaelmk@gmail.com>
2022-01-21 21:00:27 -0500

    Updates

---
+++ precache-manifest.94f2332c8fe4f9226010afa0da62341e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1cebd0473b8156d81a4510fa034151c",
+    "url": "/personal-assistant-app/index.html"
+  },
+  {
+    "revision": "8b6aa216ec229b4fbaef",
+    "url": "/personal-assistant-app/static/css/2.77ad079f.chunk.css"
+  },
+  {
+    "revision": "4ae7b178d9a0af89c24e",
+    "url": "/personal-assistant-app/static/css/main.b0f458b0.chunk.css"
+  },
+  {
+    "revision": "8b6aa216ec229b4fbaef",
+    "url": "/personal-assistant-app/static/js/2.190c3794.chunk.js"
+  },
+  {
+    "revision": "7af3935f4c3920421369256e586ba6c7",
+    "url": "/personal-assistant-app/static/js/2.190c3794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ae7b178d9a0af89c24e",
+    "url": "/personal-assistant-app/static/js/main.91155370.chunk.js"
+  },
+  {
+    "revision": "3a5d2459a81e16108259",
+    "url": "/personal-assistant-app/static/js/runtime-main.fe480bfc.js"
+  }
+]);
